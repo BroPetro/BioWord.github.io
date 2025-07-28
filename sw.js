fd706c1876cd14e7f9fb8b812ef2a087aa3ac7db
BroPetro <153943156+BroPetro@users.noreply.github.com>
2025-07-28 17:28:40 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -24,20 +24,6 @@ self.addEventListener('install', event => {
       return cache.addAll(urlsToCache);
     })
   );
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames
-          .filter(cacheName => cacheName !== CACHE_NAME)
-          .map(cacheName => caches.delete(cacheName))
-      );
-    })
-  );
-  self.clients.claim();
 });
 
 self.addEventListener('fetch', event => {
@@ -48,10 +34,4 @@ self.addEventListener('fetch', event => {
       });
     })
   );
-});
-
-self.addEventListener('message', event => {
-  if (event.data && event.data.type === 'SKIP_WAITING') {
-    self.skipWaiting();
-  }
 });
